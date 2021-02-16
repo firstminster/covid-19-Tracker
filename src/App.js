31706927f652e9c15849a85cc8f340309fd23c62
Promise Harry <chinecherempharry@gmail.com>
2021-02-16 14:20:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { FormControl, MenuItem, Select } from '@material-ui/core'
+import './App.css'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('worldwide')
+
+  // Loads when component change
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country, // United states, United kingdom
+            value: country.countryInfo.iso2, // United states, United kingdom
+          }))
+          setCountries(countries)
+        })
+    }
+
+    getCountriesData()
+  }, [])
+
+  // Handler for select
+  const onCountryChange = event => {
+    const countryCode = event.target.value
+    setCountry(countryCode)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__header'>
+        <h1>COVID_19 Tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' onChange={onCountryChange} value={country}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
